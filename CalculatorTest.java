1b6c530db76394687d1cd227da6f4b4490b1828e
Tomasz <tomaszmhoffman@gmail.com>
2023-09-12 09:10:41 +0200

    Praca domowa metody (kalkulator)

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,7 +1,7 @@
 package cwiczeniaJava;
 
 import java.util.Scanner;
-public class Calculator {
+public class CalculatorTest {
     public static void main(String[] args) {
         Scanner reader_data = new Scanner(System.in);
         System.out.print("Wprowadź pierwszą liczbę: ");
@@ -9,11 +9,14 @@ public class Calculator {
         System.out.print("Wprowadź drugą liczbę: ");
         int secondNumber = reader_data.nextInt();
 
-        int addition = firstNumber + secondNumber;
-        int subtraction = firstNumber - secondNumber;
-        int multiplication = firstNumber * secondNumber;
-        float division = firstNumber / secondNumber;
-        int mod = firstNumber % secondNumber;
+        Calculator calculator = new Calculator();
+
+
+        int addition = calculator.addition(firstNumber,secondNumber);
+        int subtraction = calculator.subtract(firstNumber,secondNumber);
+        int multiplication = calculator.multiply(firstNumber,secondNumber);
+        float division = calculator.divide(firstNumber,secondNumber);
+        int mod = calculator.modulo(firstNumber,secondNumber);
 
         System.out.println("Wynik dodawania to " + addition +"!");
         System.out.println("Wynik odejmowania to " + subtraction +"!");
