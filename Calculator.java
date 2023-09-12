ba556d18f25152a9f679b87620614842ee6fc992
Tomasz <tomaszmhoffman@gmail.com>
2023-09-12 09:11:31 +0200

    Praca domowa metody cz2 (kalkulator)

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+package cwiczeniaJava;
+
+public class Calculator {
+    public int addition(int firstNumber, int secondNumber) {
+        return firstNumber+secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber-secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber*secondNumber;
+    }
+
+    public int divide(int firstNumber, int secondNumber) {
+        return firstNumber/secondNumber;
+    }
+
+    public int modulo(int firstNumber, int secondNumber) {
+        return firstNumber%secondNumber;
+    }
+}
